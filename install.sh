f58980cdc0024a2829d18de9236ca9bb5aa0954c
ybouhjira <b.youssef91@gmail.com>
2014-04-13 23:28:13 +0100

    Copying config files during install

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ echo "Installing hostapd & dnsmasq"
 sudo apt-get install hostapd dnsmasq -y
 
 echo "Copying conf files"
-sudo cp hostapd.conf /etc/hostapd.conf
-sudo cp dnsmasq.conf /etc/dnsmasq.conf
+sudo cp ./hostapd.conf /etc/hostapd.conf
+sudo cp ./dnsmasq.conf /etc/dnsmasq.conf
